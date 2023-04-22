d2abfd741cb520b9a6c9e33ec16262cd07755815
Beatris Borisova <beti16b@gmail.com>
2023-04-22 18:28:38 +0300

    Controlled forms - setter inside each input field

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -15,69 +15,42 @@ export function Register() {
         console.log('register');
     }
 
-    const changeUsername = (e: any) => {
-        setUsername(e.target.value)
-    }
-
-    const changePassword = (e: any) => {
-        setPassword(e.target.value)
-    }
-
-    const changeRepass = (e: any) => {
-        setRepass(e.target.value)
-    }
-
-    const changeGender = (e: any) => {
-        setGender(e.target.value)
-    }
-
-    const changeBio = (e: any) => {
-        setBio(e.target.value)
-    }
-
-    const changeCitizenship = (e: any) => {
-        setCitizenship(e.target.value)
-    }
-
-    const changeRobot = (e: any) => {
-        setRobot(oldState => !Boolean(oldState))
-    }
-
     return (
         <>
             <h3>Register form</h3>
             <form onSubmit={onRegister}>
                 <div>
                     <label htmlFor='username'>Username:</label>
-                    <input type='text' id='username' name='username' value={username} onChange={changeUsername} />
+                    <input type='text' id='username' name='username' value={username} onChange={(e) => setUsername(e.target.value)} />
                 </div>
                 <div>
                     <label htmlFor='password'>Passsword:</label>
-                    <input type='password' id='password' name='password' value={password} onChange={changePassword} />
+                    <input type='password' id='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div>
                     <label htmlFor='repass'>Repeat password:</label>
-                    <input type='password' id='repass' name='repass' value={repass} onChange={changeRepass} />
+                    <input type='password' id='repass' name='repass' value={repass} onChange={(e) => setRepass(e.target.value)} />
                 </div>
                 <div>
                     <label htmlFor="gender">Gender:</label>
-                    <input type='radio' id="gender" name="gender" value='male' checked={gender === 'male'} onChange={changeGender} /> Male
-                    <input type='radio' id="gender" name="gender" value='female' checked={gender === 'female'} onChange={changeGender} /> Female
+                    <input type='radio' id="gender" name="gender" value='male' checked={gender === 'male'} onChange={(e) => setGender(e.target.value)} /> Male
+                    <input type='radio' id="gender" name="gender" value='female' checked={gender === 'female'} onChange={(e) => setGender(e.target.value)} /> Female
                 </div>
                 <div>
                     <label htmlFor="bio">Bio:</label>
-                    <textarea id='bio' name='bio' value={bio} onChange={changeBio}></textarea>
+                    <textarea id='bio' name='bio' value={bio} onChange={(e) => setBio(e.target.value)}></textarea>
                 </div>
                 <div>
                     <label>Citizenship</label>
-                    <select id="citizenship" name="citizenship" value={citizenship} onChange={changeCitizenship}>
+                    <select id="citizenship" name="citizenship" value={citizenship} onChange={(e) => setCitizenship(e.target.value)}>
                         <option value="eu">EU</option>
                         <option value="noneu">Non EU</option>
                     </select>
                 </div>
                 <div>
                     <label>Confirm you're not a robot</label>
-                    <input type='checkbox' id="robot" name="robot" checked={robot} onChange={changeRobot} />
+                    {/* <input type='checkbox' id="robot" name="robot" checked={robot} onChange={(e) => setRobot(e.target.checked)} /> */}
+                    <input type='checkbox' id="robot" name="robot" checked={robot} onChange={() => setRobot(oldState => !Boolean(oldState))} />
                 </div>
                 <input type='submit' value='Register' />
             </form>
